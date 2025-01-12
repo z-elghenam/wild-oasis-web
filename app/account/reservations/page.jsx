d39400acdfd53e0fcb3fdfc89be0e433a29baaa9
ZAKI--V2.0.0 <zelghenam@gmail.com>
2025-01-12 10:04:43 +0000

    Refactor: Replace <a> tags with <Link> Component for client-side navigation

--- app/account/reservations/page.jsx
+++ app/account/reservations/page.jsx
@@ -1,4 +1,5 @@
 import ReservationCard from "@/app/_components/ReservationCard";
+import Link from "next/link";
 
 export const metadata = {
   title: "Reservations",
@@ -17,9 +18,9 @@ export default function Page() {
       {bookings.length === 0 ? (
         <p className="text-lg">
           You have no reservations yet. Check out our{" "}
-          <a className="underline text-accent-500" href="/cabins">
+          <Link className="underline text-accent-500" href="/cabins">
             luxury cabins &rarr;
-          </a>
+          </Link>
         </p>
       ) : (
         <ul className="space-y-6">
