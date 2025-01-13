1c9e28bf65dd16fbdc17b7ea942d7ed232164956
ZAKI--V2.0.0 <zelghenam@gmail.com>
2025-01-13 10:31:57 +0000

    Setting Up NEXTAUTH

---
+++ app/_lib/auth.js
@@ -0,0 +1,20 @@
+import NextAuth from "next-auth";
+import Google from "next-auth/providers/google";
+
+// const authConfig = {
+//   providers: [
+//     Google({
+//       clientId: process.env.AUTH_GOOGLE_ID,
+//       clientSecret: process.env.AUTH_GOOGLE_SECRET,
+//     }),
+//   ],
+// };
+
+export const { handlers: {GET, POST}, auth, signIn, signOut } = NextAuth({
+  providers: [
+    Google({
+      clientId: process.env.AUTH_GOOGLE_ID,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET,
+    }),
+  ],
+});
