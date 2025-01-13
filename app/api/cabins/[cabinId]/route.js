5e968780a64725e763cbc57ffe02ccebbc77cdcb
ZAKI--V2.0.0 <zelghenam@gmail.com>
2025-01-13 10:31:05 +0000

    Create and API endPoint for testing get Cabin and we can implement in NEXT.js.

---
+++ app/api/cabins/[cabinId]/route.js
@@ -0,0 +1,16 @@
+import { getBookedDatesByCabinId, getCabin } from "@/app/_lib/data-service";
+
+export async function GET(request, { params }) {
+  const { cabinId } = await params;
+
+  try {
+    const [cabin, bookedDates] = await Promise.all([
+      getCabin(cabinId),
+      getBookedDatesByCabinId(cabinId),
+    ]);
+
+    return Response.json({cabin, bookedDates});
+  } catch {
+    return Response.json({ messge: "not Found" });
+  }
+}
