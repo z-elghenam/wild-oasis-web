132242908dcb31236b42d570515d3d7c78821027
ZAKI--V2.0.0 <zelghenam@gmail.com>
2025-01-28 13:23:16 +0000

    with adding this functionally the project was final finished.

--- app/_components/ReservationForm.jsx
+++ app/_components/ReservationForm.jsx
@@ -10,8 +10,8 @@ function ReservationForm({ cabin, user }) {
   // CHANGE
   const { maxCapacity, regularPrice, discount, id } = cabin;
 
-  const startDate = range.from;
-  const endDate = range.to;
+  const startDate = range?.from;
+  const endDate = range?.to;
 
   const numNights = differenceInDays(endDate, startDate);
   const cabinPrice = numNights * (regularPrice - discount);
@@ -30,7 +30,7 @@ function ReservationForm({ cabin, user }) {
   );
 
   return (
-    <div className="scale-[1.01]">
+      <div className="scale-[1.01]">
       <div className="bg-primary-800 text-primary-300 px-16 py-2 flex justify-between items-center">
         <p>Logged in as</p>
 
@@ -47,9 +47,9 @@ function ReservationForm({ cabin, user }) {
       </div>
 
       <form
-        // action={createReservationWithData}
+        // action={createBookingWithData}
         action={async (formData) => {
-          await createReservationWithData(formData);
+          await createBookingWithData(formData);
           resetRange();
         }}
         className="bg-primary-900 py-10 px-16 text-lg flex gap-5 flex-col"
@@ -86,7 +86,7 @@ function ReservationForm({ cabin, user }) {
         </div>
 
         <div className="flex justify-end items-center gap-6">
-          {!startDate && !endDate ? (
+          {!(startDate && endDate) ? (
             <p className="text-primary-300 text-base">
               Start by selecting dates
             </p>
